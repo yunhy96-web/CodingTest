3dff03b9b612af2fa6f8b2ef1c4e0473daaa661b
huiyeong yun <yunhy96@huiyeongui-MacBookPro.local>
2024-02-04 18:11:36 +0900

    유기농

--- algo/src/백준_유기농배추.java
+++ algo/src/백준_유기농배추.java
@@ -7,9 +7,12 @@ public class 백준_유기농배추 {
     private static int[][]map;
     private static boolean[][]visit;
     private static int count=0;
+    private static int[]dx = {-1, 0, 1, 0};
+    private static int[]dy = {0, 1, 0, -1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for(int Test =0; Test < T; Test++){ //총 2번 돌려야함
             StringTokenizer st = new StringTokenizer(br.readLine());
             int M = Integer.parseInt(st.nextToken()); // 가로 길이
@@ -23,16 +26,27 @@ public class 백준_유기농배추 {
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
                 map[y][x] = 1; //배추의 위치
-                if(!visit[y][x]){
-                    dfs(map, visit);
-                    count++;
+            }
+            for(int i=0; i<N; i++){
+                for(int j=0; j<M ; j++){
+                    if(!visit[i][j] && map[i][j] == 1){
+                        dfs(map, visit, i, j);
+                        count++;
+                    }
                 }
-                dfs(map, visit); //만약 이미 visit이 되어있는 상태라면 넘어가는 로직이 안에 들어있어야함
-                //visit이 안되어있는 상태면 dfs를 돌리면서 count를 ++
+            }
+            sb.append(count+"\n");
+        }
+        System.out.println(sb);
+    }
+    private static void dfs(int[][]map, boolean[][]visit, int curN, int curM){
+        visit[curN][curM] = true; //방문처리 해야함
+        for(int i = 0; i<4; i++){
+            int nextN = curN + dy[i];
+            int nextM = curM + dx[i];
+            if(nextN >=0 && nextN < map.length && nextM >=0 && nextM < map[0].length && map[nextN][nextM] ==1 && !visit[nextN][nextM]){
+                dfs(map, visit, nextN, nextM);
             }
         }
-    }
-    private static void dfs(int[][]map, boolean[][]visit){
-
     }
 }
